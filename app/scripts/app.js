63e199234435d3249e9cc642f5249d2bdab997ec
Mohamed Hesham Alma'zon <m.hesham@outlook.com>
2018-08-19 07:23:39 +0200

    Add files via upload

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name newModuleQuizApp
+ * @description
+ * # newModuleQuizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('newModuleQuizApp', ['legoBricks']);
+
